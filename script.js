ad2d91c68554e6182e517542e14abbbb70017d9b
sujipinku07 <sujipinku07@gmail.com>
2025-05-24 21:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+    let count = 0;
+    while (n >= 5) {
+        n = Math.floor(n / 5);
+        count += n;
+    }
+    return count;
+//your JS code here. If required.
 }
 
 const input = prompt("Enter a number");
